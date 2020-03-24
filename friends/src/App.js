7968eb243c03168b17adf8443810dee69351d09a
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-24 16:22:36 -0400

    private route set up, protects friends list

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import {Link, Route} from 'react-router-dom';
 import './App.css';
 import { Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute'
 import LoginForm from './components/LoginForm';
 import { FriendsContext } from './contexts/FriendsContext';
+import FriendsList from './components/FriendsList';
 
 function App() {
 
@@ -12,9 +14,11 @@ function App() {
       <h1>FRIENDS</h1>
       <nav>
         <Link to='/login' >Login</Link>
+        <Link to='/friends-list' >Friends List</Link>
       </nav>
       <FriendsContext.Provider>
         <Switch>
+          <PrivateRoute exact path='/friends-list' component={FriendsList} />
           <Route path='/login' component={LoginForm} />
         </Switch>
       </FriendsContext.Provider>
