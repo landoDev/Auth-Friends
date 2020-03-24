9537555e970ed3b9f989ab2425b0f117680d8c31
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-24 16:01:58 -0400

    credentials in login have successful post and save token to local storage

--- friends/src/components/LoginForm.js
+++ friends/src/components/LoginForm.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+
 
 const LoginForm = () =>{
+    const [isLoading, setIsLoading] = useState(false)
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChanges = e =>{
+        e.preventDefault();
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/login', credentials)
+        .then(res=>{
+            console.log(res);
+            localStorage.setItem('token', JSON.stringify(res.data.payload))
+            //props.history.push('/friends-list)
+        })
+    }
+
+    console.log('change made', credentials)
     return(
         <div>
-            <form>
-                <input type='text'></input>
-                <input type='password'></input>
-                <button>Login</button>
+            <form onSubmit={handleLogin}>
+                <input type='text' placeholder='Username' name='username' onChange={handleChanges}></input>
+                <input type='password' placeholder='Password' name='password' onChange={handleChanges}></input>
+                <button type='submit'>Login</button>
             </form>
         </div>
     )
