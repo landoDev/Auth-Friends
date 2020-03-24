0f34bf1fb571c24c515ba8dd60ce21ec6493d6d8
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-24 15:13:10 -0400

    form renders to page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Link, Route} from 'react-router-dom';
 import './App.css';
+import { Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FRIENDS</h1>
+      <nav>
+        <Link to='/login' >Login</Link>
+      </nav>
+      <Switch>
+        <Route path='/login' component={LoginForm} />
+      </Switch>
     </div>
   );
 }
---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoginForm = () =>{
+    return(
+        <div>
+            <form>
+                <input type='text'></input>
+                <input type='password'></input>
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
